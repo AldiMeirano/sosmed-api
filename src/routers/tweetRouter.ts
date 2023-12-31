3b519f4876f676d81cbc4b1944ebdf08bd6819ff
Aldi Meirano <116731457+AldiMeirano@users.noreply.github.com>
2023-12-31 21:16:18 +0700

    feat: fiture delete tweet (#6)

--- src/routers/tweetRouter.ts
+++ src/routers/tweetRouter.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { addTweetController } from "../controllers/tweetsControllers/addTweetController";
 import { getTweetController } from "../controllers/tweetsControllers/getTweetControllers";
+import { deleteTweetController } from "../controllers/tweetsControllers/deleteTweetController";
 
 const router = express.Router();
 
 router.post("/add-tweet", addTweetController);
 router.get("/", getTweetController);
+router.delete("/:id", deleteTweetController);
 export default router;
