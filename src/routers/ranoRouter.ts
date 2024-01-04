1a569ac1794336804b2686fd451cdebbecc7cc21
AldiMeirano <aldymerno173@gmail.com>
2024-01-05 01:27:47 +0700

    feat: Membuat other profile api

--- src/routers/ranoRouter.ts
+++ src/routers/ranoRouter.ts
@@ -3,9 +3,11 @@ import { registerRanoController } from "../controllers/registerRanoController";
 import { loginRanoController } from "../controllers/loginRanoController";
 import { keepLoginController } from "../controllers/keepLoginController";
 import { verifyToken } from "../middleware/jwtVerifyToken";
+import { findUserIdController } from "../controllers/findUserIdController";
 const router = express.Router();
 
 router.post("/register", registerRanoController);
 router.post("/login", loginRanoController);
 router.get("/keeplogin", verifyToken, keepLoginController);
+router.get("/:id", findUserIdController);
 export default router;
